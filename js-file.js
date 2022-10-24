93be1de2c7d4311a87167f4adc4421d67077f48c
CousinJoey <cousinjoeyllc@gmail.com>
2022-10-24 14:22:16 -0400

    Dynamic div creation and adjust size button with functionality

--- js-file.js
+++ js-file.js
@@ -2,3 +2,37 @@ function placeHolder(x) {
     console.log("this function does nothing")
 }
 
+globalPara = 0
+
+const adjustSize = document.querySelector("#size-btn")
+
+adjustSize.addEventListener("click", popup);
+
+
+
+function popup(x) {
+    const clearGrid = document.getElementById("gridDisplay");
+    while (clearGrid.lastElementChild) {
+        clearGrid.removeChild(clearGrid.lastElementChild);
+    }
+    let para = prompt("Please enter a value", );
+    let subPara = parseInt(para)
+    globalPara = subPara
+    let finalPara = (subPara * subPara)
+    dynamicGrid(finalPara)
+}
+
+
+const gridDisplay = document.querySelector("#gridDisplay")
+
+function dynamicGrid(gridSize) {
+    for(let i=0;i<gridSize;i++) {
+        const temp = document.createElement("div")
+        gridDisplay.append(temp);
+        temp.textContent = `${i+1}`;
+    }
+    console.log(globalPara)
+}
+
+gridDisplay.style.setProperty('grid-template-columns', 10, 1)
+gridDisplay.style.setProperty('grid-template-rows', 10, 1)
